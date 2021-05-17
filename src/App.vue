ae11fb3621368fdc9963f3cec092e835cbbaadac
ShrineOX <thitheluc.la@gmail.com>
2021-05-17 13:34:09 +0700

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,19 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+
+    };
+  },
+};
 </script>
 
 <style>
